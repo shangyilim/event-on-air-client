8793c8afe1d8cd24dda0d24da84bff30e95408ab
Lim Shang Yi <shangyilim@gmail.com>
2019-04-14 05:01:10 +0000

    environment updates

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "apiKey",
+    authDomain: "authDomain",
+    databaseURL: "databaseURL",
+    projectId: "projectId",
+    storageBucket: "storageBucket",
+    messagingSenderId: "messagingSenderId"
+  }
+};
